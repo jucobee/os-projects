cd1d14745ddd77440eb172d625e9d16d7e9c28df
jucobee <jucobee@gmail.com>
2024-04-15 01:34:10 -0700

    working on forking

--- unix-shell/processes-shell/wish.cpp
+++ unix-shell/processes-shell/wish.cpp
@@ -4,16 +4,34 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
   // repeat forever
-  while (1) {
+  while (true) {
     string cmd;
 
     cout << "wish> ";
-    getline(cin, cmd);
+    getline(cin, cmd); // read input from terminal
+
+    pid_t pid = fork();
+    if (pid == 0) {
+      /* Child */
+      // TODO
+      // execv(const char *path, char *const *argv)
+
+    } else if (pid > 0) {
+      /* Parent */
+      int status;
+      waitpid(pid, &status, 0);
+
+    } else {
+      cerr << "fork error" << endl;
+      return 1;
+    }
   }
+  return 0;
 }
