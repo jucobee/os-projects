00cea39dd8638aeec4bda52e3de418059cd70309
jucobee <jucobee@gmail.com>
2024-04-16 11:16:38 -0700

    ls and exit working

--- unix-shell/processes-shell/wish.cpp
+++ unix-shell/processes-shell/wish.cpp
@@ -9,19 +9,37 @@
 
 using namespace std;
 
+int built_in_command(string cmd) {
+  if (cmd == "exit")
+    exit(0);
+
+  // return 0 back to main if command is not built in
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
+  // Path to directory with all programs
+  string path = "/bin/";
+
   // repeat forever
   while (true) {
     string cmd;
-
     cout << "wish> ";
     getline(cin, cmd); // read input from terminal
 
+    // attempt to execute built in command and restart while loop
+    if (built_in_command(cmd))
+      continue;
+
+    // make new process for command
     pid_t pid = fork();
     if (pid == 0) {
       /* Child */
-      // TODO
-      // execv(const char *path, char *const *argv)
+      path += cmd;
+
+      // Arguments to be passed to the program
+      const char *args[] = {path.c_str(), NULL};
+      execv(path.c_str(), const_cast<char *const *>(args));
 
     } else if (pid > 0) {
       /* Parent */
